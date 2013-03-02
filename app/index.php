2fae0aa77addaa56cb01d52d9d4296eb86cd4823
Hugo N. Nomura <hugonomura@gmail.com>
2013-03-02 19:49:03 -0300

    Tirando debugger do index.

--- app/index.php
+++ app/index.php
@@ -1,9 +1,5 @@
 <?php
 
-// dbg
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-
   require_once('controllers/controller_recado.php');
   require_once('setup.php');
   $smarty = new Smarty_LivroVisitas;
